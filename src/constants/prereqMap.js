a700440b7476cf38e6a1e8ed083f7b32c5b9d0ea
Frank Moore <moorewf@wfu.edu>
2024-09-12 09:02:28 -0400

    Added statistics courses and prerequisites.

--- src/constants/prereqMap.js
+++ src/constants/prereqMap.js
@@ -104,6 +104,48 @@ export default [
         },
 	"mth392": {
             "Prereq": "MTH 391"
-        }
+        },
+	"sta112": {
+	    "Prereq": "STA 111"
+	},
+	"sta214": {
+	    "Prereq": "MTH 111 and STA 112"
+	},
+	"sta247": {
+	    "Prereq": "STA 112"
+	},
+	"sta310": {
+	    "Prereq": "MTH 112"
+	},
+	"sta311": {
+	    "Prereq": "STA 310 or MTH 357"
+	},
+	"sta312": {
+	    "Prereq": "(MTH 121 or MTH 205) and (STA 310 or MTH 357)"
+	},
+	"sta352": {
+	    "Prereq": "MTH 117 or MTH 121 or MTH 205"
+	},
+	"sta362": {
+	    "Prereq": "STA 112 and (MTH 121 or MTH 205)"
+	},
+	"sta363": {
+	    "Prereq": "STA 112 and (MTH 121 or MTH 205)"
+	},
+	"sta364": {
+	    "Prereq": "STA 112 and (STA 310 or MTH 357)"
+	},
+	"sta365": {
+	    "Prereq": "STA 112 and (STA 310 or MTH 357)"
+	},
+	"sta368": {
+	    "Prereq": "STA 112 and (STA 310 or MTH 357)"
+	},
+	"sta382": {
+	    "Prereq": "STA 363"
+	},
+	"sta392": {
+	    "Prereq": "STA 391"
+	}
     }
 ];
